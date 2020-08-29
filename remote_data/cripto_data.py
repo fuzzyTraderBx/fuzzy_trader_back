d5932aa5c67c678b9b888b40a9363c5dded21adf
Leticia de Souza <leticia.souza212@gmail.com>
2020-08-29 19:06:23 -0300

    Retrieve stock exchange from API

--- remote_data/cripto_data.py
+++ remote_data/cripto_data.py
@@ -1,3 +1,6 @@
+import datetime
+
+import requests
 from bitcoinaverage import RestfulClient
 
 from models.investment import InvestmentModel
@@ -5,8 +8,13 @@ from models.investment import InvestmentModel
 public_key = 'YjUxZDI3MmE3ZmU3NDQwNWE4NmJjMGZjNjgzNTIyMDc'
 secret_key = 'YTZmYzQ5MTRiMDYxNDFlZjhiNzAwYzA2NmQ1MmJmZTU2NmJjZTE2YmRjOTg0YzA3ODg4ZTAyZTAzYzZmMGEwZQ'
 
+market_stack_url = 'http://api.marketstack.com/v1/tickers/'
+
 restful_client = RestfulClient(secret_key, public_key)
 currencies = ['BTCUSD', 'ETHUSD', 'LTCUSD']  # Some currencies
+stocks = ['MSFT', 'AAPL', 'AMZN', 'GOOG', 'GOOGL', 'FB', 'VOD', 'INTC', 'CMCSA', 'PEP', 'ADBE', 'CSCO', 'NVDA', 'NFLX',
+          'TSLA', 'COST', 'PYPL', 'AMGN', 'SNY', 'ASML']
+
 
 def get_cripto(max_price):
 
@@ -25,10 +33,34 @@ def get_cripto(max_price):
 
 
 def retrieve_investment(investment_key):
+
+    """
+    I need to search into the api of actions a action "investment_key"
+    :param investment_key:
+    :return:
+    """
+
     if investment_key in currencies:
         investment = restful_client.ticker_global_per_symbol(investment_key)
         investment = InvestmentModel(investment_key, investment["last"], True)
 
         return investment
+    elif investment_key in stocks:
+        # Searching data from stock api
+
+        params = {
+            'access_key': '7afb61a152b7ecb2211f70f75508a255',
+        }
+
+        response = requests.get(market_stack_url + investment_key + '/eod', params=params)
+        if response.status_code != 400:
+            response = response.json()
+            response = response['data']
+            last_days = response['eod']
+            print(response['symbol'])
+            if last_days:
+                investment = InvestmentModel(response['symbol'], last_days[0]['close'], False)
+
+                return investment
 
     return None
